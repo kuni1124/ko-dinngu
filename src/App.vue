bd9eec90515389832f24bce3a2de69e47b57b441
梅木邦彦 <umekikunihiko@umekikunihikonoMacBook-Pro.local>
2020-10-19 21:17:55 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+
+  
+</script>
+
+<style>
+.header {
+  background-color: blanchedalmond;
+  
+  
+}
+
+.left_box{
+  float:left;
+  margin:0px 0px;
+}
+.right_box{
+  float:left;
+  
+  
+}
+.support-list ul{
+  list-style: none;
+  margin-bottom:0px;
+  margin-left:80px;
+}
+.support-list li{
+  float:left;
+  margin-right:40px;
+  
+  
+}
+.first{
+  margin-left:50px;
+}
+.search{
+  margin-left:80px;
+  
+}
+.sidebar-form{
+  height:30px;
+  
+}
+.button{
+  height:35px;
+  margin-right:15px;
+}
+.login-bar{
+  float:left;
+  margin-top:0px;
+}
+.search li{
+  float:left;
+  list-style:none;
+  
+}
+.left-bar{
+ margin-top:0px;
+}
+.navi li{
+ float:left;
+ list-style:none;
+ margin-right:60px;
+ border-right:slategrey solid;
+ background-color: steelblue;
+ 
+
+ 
+
+
+}
+</style>
